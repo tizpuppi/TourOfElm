ab214d877971468c806ad98fa18036f07cf6aa17
Puppi Tiziano <AFG548@win.corp.com>
2016-10-24 18:11:34 +0200

    Initial set up of Elm using gulp

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require('gulp');
+var elm  = require('gulp-elm');
+var uglify = require('gulp-uglify');
+var fs     = require('fs');
+var gutil  = require('gulp-util');
+var del = require('del');
+var webserver = require('gulp-webserver');
+
+gulp.task('default', ['serve']);
+
+gulp.task('serve', ['build','watch','server']);
+
+gulp.task('build', ['build:elm','build:files']);
+
+var paths = {
+  src : 'src',
+  out : 'assets'
+};
+
+// elm-make --yes
+gulp.task('build:init', elm.init);
+
+gulp.task('build:elm', ['build:init'], function(){
+  return gulp.src(paths.src + '/**/*.elm')
+    .pipe(elm.bundle('app.js',{warn:true}))
+    .on('error', gutil.log)
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.out + '/'));
+});
+
+gulp.task('build:files', function(){
+  return gulp.src([paths.src + '/index.html', paths.src + '/styles.css', paths.src + '/favicon.ico'])
+    .pipe(gulp.dest(paths.out + '/'))
+});
+
+
+gulp.task('clean', function(cb){
+  del([paths.out +"/**/*"], {force: true} , cb)
+});
+
+gulp.task('server', function() {
+  gulp.src(paths.out)
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: false,
+      open: true,
+      fallback: 'index.html'
+    }));
+});
+
+gulp.task('watch', function(){
+  gulp.watch([paths.src +'/**/*.elm'],['build:elm']);
+  gulp.watch([paths.src +'/*.html'],['build:files'])
+});
